2462a375a8f9b44527555ded785d329e6c6b7f2a
Joe Danziger <joe@ticc.net>
2025-04-25 14:58:56 -0400

    add multibar

--- scripts/modules/progress/cliProgressFactory.js
+++ scripts/modules/progress/cliProgressFactory.js
@@ -11,3 +11,15 @@ export function newSingle(opts = {}) {
 		cliProgress.Presets.shades_classic
 	);
 }
+
+export function newMultiBar(opts = {}) {
+	return new cliProgress.MultiBar(
+		{
+			clearOnComplete: false,
+			hideCursor: true,
+			barsize: 40,
+			...opts
+		},
+		cliProgress.Presets.shades_classic
+	);
+}
